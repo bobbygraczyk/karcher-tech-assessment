742235ebfc7ac960e984d041cd69071bfafb37a8
Bobby Graczyk <bobbygraczyk@gmail.com>
2018-12-11 12:53:11 -0500

    Header content added and roughly styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/header.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Hi, Karcher Group!
-        </header>
+        <Header />
       </div>
     );
   }
