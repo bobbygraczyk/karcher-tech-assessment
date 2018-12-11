2f68954b68ff2a4693cc42d53a7b3868e2201abf
Bobby Graczyk <bobbygraczyk@gmail.com>
2018-12-11 13:50:02 -0500

    Blocking and adding some assets for main content

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Swiper from 'react-id-swiper';
 import stock1 from '../assets/stock1.jpg';
 import stock2 from '../assets/stock2.jpg';
+import kohler from '../assets/kohler.png';
+import moen from '../assets/moen.png';
+import symmons from '../assets/symmons.png';
+import elkay from '../assets/elkay.png';
+import texture from '../assets/texture.png';
 import '../App.css';
 
 class Main extends Component {
@@ -20,11 +25,21 @@ class Main extends Component {
     }
     return (
       <div className="main-content">
-        <div className="content-frame">
+        <div className="swiper-frame">
             <Swiper {...params}>
                 <img src={stock1} />
                 <img src={stock2} />
             </Swiper>
+        </div>
+        <div id="brand-logos">
+            <div className="content-frame">
+                <img src={kohler} />
+                <img src={moen} />
+                <img src={symmons} />
+                <img src={elkay} />
+            </div>
+        </div>
+        <div id="cards">
         </div>
       </div>
     );
