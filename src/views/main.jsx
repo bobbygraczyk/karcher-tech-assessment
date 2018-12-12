0396f705ca4b9893f2922d91e4e0ee26d723725e
Bobby Graczyk <bobbygraczyk@gmail.com>
2018-12-12 13:04:23 -0500

    Text styling on welcome text

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -72,12 +72,12 @@ class Main extends Component {
               <button>FIND IT HERE</button>   
             </div>
             <div id="welcome-text">
-              <h4>Welcome to Cleveland Plumbing Supply</h4><br />
+              <h4>Welcome to Cleveland Plumbing Supply</h4>
               <small>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inveveritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspeatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
-              </small>
+              </small><br />
             </div>
             <div id="showroom-links">
-              <h4>Showroom Location:</h4><br />
+              <h4>Showroom Location:</h4>
               <a href="">Cleveland Showroom</a><br />
               <a href="">Chagrin Falls Showroom</a><br />
               <a href="">Medina Showroom</a><br />
